86612536e6f43e20a63558a9e4db36ed32d796c6
Grace Owen <graceowen@brb-01878.apn.wlan.private.upenn.edu>
2024-07-22 11:56:49 -0400

    added some js

--- gamePage.js
+++ gamePage.js
@@ -17,6 +17,9 @@ let cardItems = [
     {value: "light", background: "light.jpg", flipped: false},
 ];
 
+let correctGuesses = 0;
+let wrongGuesses = 0;
+
 let items = document.getElementsByClassName("item");
 for (let item of items){
     item = setValues()
@@ -38,7 +41,6 @@ for (let item of items){
 }
 
 let clickedCards = [];
-
 function checkSame(){
     if (clickedCards.length == 1){
         let card1 = clickedCards[0];
@@ -46,9 +48,33 @@ function checkSame(){
         if (card1.value == card2.value){
             card1.flipped = true;
             card2.flipped = true;
+            correctGuesses++;
+            redrawGrid();
+        } else{
+            card1.flipped = false;
+            card2.flipped = false;
+            wrongGuesses++;
+            redrawGrid();
         }
     } else {
         clickedCards.append(this);
+        this.flipped = true;
+        redrawGrid();
     }
 }
+
+function redrawGrid(){
+    let numLeft = 0;
+    for (let item of items){
+        if (item.flipped == true){
+            numLeft++;
+            item.backgroundColor = blue;
+        }
+    }
+    if (numLeft == items.length){
+        document.getElementById("container").textContent = "Finished!!";
+    }
+}
+
+
 
