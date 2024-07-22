a79a8362ddb9515c65cd3499ffeaffded2c7f7dd
Marquez Rosario <142691915+marquezrosario@users.noreply.github.com>
2024-07-22 16:22:28 -0400

    Update gamePage.js

--- gamePage.js
+++ gamePage.js
@@ -57,7 +57,7 @@ function initializeGame() {
         updateProgressBar();
         updateGuesses();
         startTimer(); // Start the timer
-    }, 5000); // 5 seconds preview time
+    }, 3000); // 5 seconds preview time
 }
 
 function startTimer() {
