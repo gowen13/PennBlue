f14d3f8ebfeaebc20e0c4aa9c23060ab2a771cf9
Grace Owen <graceowen@brb-01878.apn.wlan.private.upenn.edu>
2024-07-24 11:17:40 -0400

    added best score

--- endPage.js
+++ endPage.js
@@ -1,6 +1,14 @@
 let correctGuessesTotal = localStorage.getItem('correctGuessesTotal') ? parseInt(localStorage.getItem('correctGuessesTotal')) : 0;
 let wrongGuessesTotal = localStorage.getItem('wrongGuessesTotal') ? parseInt(localStorage.getItem('wrongGuessesTotal')) : 0;
 
+let bestScore = localStorage.getItem('bestScore') ? parseInt(localStorage.getItem('bestScore')) : 0;
+
 document.getElementById('incorrectValueTotal').textContent = wrongGuessesTotal;
 document.getElementById('correctValueTotal').textContent = correctGuessesTotal;
 document.getElementById('scoreCounter').textContent = correctGuessesTotal - wrongGuessesTotal;
+
+if (bestScore < (correctGuessesTotal - wrongGuessesTotal)){
+    bestScore = correctGuessesTotal - wrongGuessesTotal;
+    localStorage.setItem('bestScore', bestScore);
+}
+document.getElementById('bestScoreCounter').textContent = bestScore;
